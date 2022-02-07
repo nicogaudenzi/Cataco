517d7f103452d09e0123af8104e32ad4d9be23fd
nicogaufer <ngaudenzi@amerike.edu.mx>
2022-02-06 20:49:14 -0600

    Finished Chapter 5

--- Overworld.js
+++ Overworld.js
@@ -3,28 +3,31 @@ class Overworld{
         this.element = config.element;
         this.canvas = this.element.querySelector(".game-canvas");
         this.ctx = this.canvas.getContext("2d");
+        this.map = null;
     }
 
-    init(){
-        const x=5;
-        const y=6;
+    startGameLoop(){
+        const step = ()=>{
+            this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height)
+            this.map.drawLowerImage(this.ctx)
+            Object.values(this.map.gameObjects).forEach(object =>{
+                object.update({
+                    arrow:this.directionInput.direction
+                });
+                object.sprite.draw(this.ctx)
+            });
+            this.map.drawUpperImage(this.ctx);
 
-        const image = new Image();
-        image.onload = () =>{
-            this.ctx.drawImage(image,0,0);
-        };
-        image.src = "/images/maps/DemoLower.png";
-
-        const shadow = new Image();
-        shadow.onload = ()=>{
-                this.ctx.drawImage(shadow,0,0,32,32,x*16-8,y*16-18,32,32);
+            requestAnimationFrame(()=>{
+                step();
+            })
         }
-        shadow.src="/images/characters/shadow.png"
-        
-        const hero = new Image();
-        hero.onload = () =>{
-            this.ctx.drawImage(hero,0,0,32,32,x*16-8,y*16-18,32,32);
-        };
-        hero.src = "/images/characters/people/Char.png";
+        step();
+    }
+    init(){
+        this.map = new OverworldMap(window.OverworldMaps.DemoRoom)
+        this.directionInput= new DirectionInput();
+        this.directionInput.init();
+        this.startGameLoop();
     }
 }
---
+++ Sprite.js
@@ -0,0 +1,76 @@
+class Sprite{
+    constructor(config){
+        this.image = new Image();
+        this.image.src = config.src;
+        this.image.onload = ()=>{this.isLoaded = true}
+        
+        //Shadow
+        this.shadow = new Image();
+        this.useShadow=true;
+        
+        if(this.useShadow){
+            this.shadow.src = "/images/characters/shadow.png";
+        }
+        this.shadow.onload = ()=>{
+            this.isShadowLoaded = true;
+        }
+        this.animations = config.animations ||{
+            "idle-down" : [ [0,0] ],
+            "idle-right": [ [0,1] ],
+            "idle-up"   : [ [0,2] ],
+            "idle-left" : [ [1,3] ],
+            "walk-down" : [ [1,0],[0,0],[3,0],[0,0], ],
+            "walk-right": [ [1,1],[0,1],[3,1],[0,1], ],
+            "walk-up"   : [ [1,2],[0,2],[3,2],[0,2], ],
+            "walk-left" : [ [1,3],[0,3],[3,3],[0,3], ]
+    }
+    this.currentAnimation =config.currenAnimation||"idle-down";
+    this.currentAnimationFrame = 0;
+
+    this.animationFrameLimit = config.animationFrameLimit||8;
+    this.animationFrameProgress = this.animationFrameLimit;
+        //Reference the gameObject
+    this.gameObject = config.gameObject;
+    }
+    get frame(){
+       
+        let anim =this.animations[this.currentAnimation];
+        return anim[this.currentAnimationFrame] ;
+    }
+
+    setAnimation(key){
+        if(this.currentAnimation!==key){
+            this.currentAnimation=key;
+            this.currentAnimationFrame =0;
+            this.animationFrameProgress = this.animationFrameLimit;
+        }
+    }
+    updateAnimationProgress(){
+        if(this.animationFrameProgress>0){
+            this.animationFrameProgress-=1;
+            return
+        }
+
+        this.animationFrameProgress = this.animationFrameLimit;
+
+        this.currentAnimationFrame +=1;
+        if(this.frame=== undefined){
+            this.currentAnimationFrame=0;
+        }
+    }
+    draw(ctx){
+        const x = this.gameObject.x  -8;
+        const y = this.gameObject.y  -18;
+
+        this.isShadowLoaded && ctx.drawImage(this.shadow, x, y)
+        const [frameX,frameY]=this.frame;
+        this.isLoaded && ctx.drawImage(this.image,
+            frameX*32,frameY*32,
+            32,32,
+            x,y,
+            32,32)
+        
+        this.updateAnimationProgress();
+    }
+   
+}
